351f846d44481231ac39857ee44f6c40fed37388
Ivan Adi Saputra <ivanadisaputra34@gmail.com>
2022-03-03 18:37:28 +0700

    Add header

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,30 @@
-<h1>Charity</h1>
+<script>
+	import CharityList from './CharityList.svelte';
+	let title = "Charity";
+	setTimeout(function() {
+		title = "Donation"
+	}, 2000);
+</script>
+
+ 
+
+<style>
+	h1{
+		font-size: 3em;
+		color: #e2071c;
+	}
+	:global(div){
+		border: 0.5em solid #1a1a1a;
+		padding: 1em;
+	}
+</style>
+
+<svelte:head>
+	<link rel="stylesheet" href="https://cdn.rawgit.com/kimeiga/bahunya/css/bahunya-0.1.3.css" rel="stylesheet"/>
+</svelte:head>
+
+
+<div>
+	<h1>{title}</h1> 
+	<CharityList /> 
+</div> 
