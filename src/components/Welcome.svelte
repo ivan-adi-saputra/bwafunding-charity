3026fead19f0f5153202d966cf6529de4f769cd4
Ivan Adi Saputra <ivanadisaputra34@gmail.com>
2022-03-05 14:38:22 +0700

    Convert HTML to svelte components

---
+++ src/components/Welcome.svelte
@@ -0,0 +1,91 @@
+<section class="xs-welcome-slider">
+    <div class="xs-banner-slider owl-carousel owl-loaded owl-drag">
+        <!-- .xs-welcome-content END -->
+        <!-- .xs-welcome-content END -->
+        <!-- .xs-welcome-content END -->
+    <div class="owl-stage-outer"><div class="owl-stage" style="transform: translate3d(-1278px, 0px, 0px); transition: all 0s ease 0s; width: 4476px;"><div class="owl-item cloned" style="width: 639.333px;"><div class="xs-welcome-content" style="background-image: url(assets/images/slide2.png);">
+            <div class="container">
+                <div class="xs-welcome-wraper color-white">
+                    <h2>Let's free the nature at all</h2>
+                    <p>Hundreds of thousands of children experiencing or witnessing assault <br> and other
+                        gender-based violence.</p>
+                    <a href="#popularcause" class="btn btn-outline-primary">
+                        View Causes
+                    </a>
+                </div><!-- .xs-welcome-wraper END -->
+            </div><!-- .container end -->
+            <div class="xs-black-overlay"></div>
+        </div></div><div class="owl-item cloned" style="width: 639.333px;"><div class="xs-welcome-content" style="background-image: url(assets/images/slide3.png);">
+            <div class="container">
+                <div class="xs-welcome-wraper color-white">
+                    <h2>Help us in big mission to rescue</h2>
+                    <p>Hundreds of thousands of children experiencing or witnessing assault <br> and other
+                        gender-based violence.</p>
+                    <a href="#popularcause" class="btn btn-outline-primary">
+                        View Causes
+                    </a>
+                </div><!-- .xs-welcome-wraper END -->
+            </div><!-- .container end -->
+            <div class="xs-black-overlay"></div>
+        </div></div><div class="owl-item animated owl-animated-in fadeIn active" style="width: 639.333px;"><div class="xs-welcome-content" style="background-image: url(assets/images/slide1.png);">
+            <div class="container">
+                <div class="xs-welcome-wraper color-white">
+                    <h2>Hunger is stalking the globe</h2>
+                    <p>Hundreds of thousands of children experiencing or witnessing assault <br> and other
+                        gender-based violence.</p>
+                    <a href="#popularcause" class="btn btn-outline-primary">
+                        View Causes
+                    </a>
+                </div><!-- .xs-welcome-wraper END -->
+            </div><!-- .container end -->
+            <div class="xs-black-overlay"></div>
+        </div></div><div class="owl-item" style="width: 639.333px;"><div class="xs-welcome-content" style="background-image: url(assets/images/slide2.png);">
+            <div class="container">
+                <div class="xs-welcome-wraper color-white">
+                    <h2>Let's free the nature at all</h2>
+                    <p>Hundreds of thousands of children experiencing or witnessing assault <br> and other
+                        gender-based violence.</p>
+                    <a href="#popularcause" class="btn btn-outline-primary">
+                        View Causes
+                    </a>
+                </div><!-- .xs-welcome-wraper END -->
+            </div><!-- .container end -->
+            <div class="xs-black-overlay"></div>
+        </div></div><div class="owl-item" style="width: 639.333px;"><div class="xs-welcome-content" style="background-image: url(assets/images/slide3.png);">
+            <div class="container">
+                <div class="xs-welcome-wraper color-white">
+                    <h2>Help us in big mission to rescue</h2>
+                    <p>Hundreds of thousands of children experiencing or witnessing assault <br> and other
+                        gender-based violence.</p>
+                    <a href="#popularcause" class="btn btn-outline-primary">
+                        View Causes
+                    </a>
+                </div><!-- .xs-welcome-wraper END -->
+            </div><!-- .container end -->
+            <div class="xs-black-overlay"></div>
+        </div></div><div class="owl-item animated owl-animated-in fadeIn cloned" style="width: 639.333px;"><div class="xs-welcome-content" style="background-image: url(assets/images/slide1.png);">
+            <div class="container">
+                <div class="xs-welcome-wraper color-white">
+                    <h2>Hunger is stalking the globe</h2>
+                    <p>Hundreds of thousands of children experiencing or witnessing assault <br> and other
+                        gender-based violence.</p>
+                    <a href="#popularcause" class="btn btn-outline-primary">
+                        View Causes
+                    </a>
+                </div><!-- .xs-welcome-wraper END -->
+            </div><!-- .container end -->
+            <div class="xs-black-overlay"></div>
+        </div></div><div class="owl-item cloned" style="width: 639.333px;"><div class="xs-welcome-content" style="background-image: url(assets/images/slide2.png);">
+            <div class="container">
+                <div class="xs-welcome-wraper color-white">
+                    <h2>Let's free the nature at all</h2>
+                    <p>Hundreds of thousands of children experiencing or witnessing assault <br> and other
+                        gender-based violence.</p>
+                    <a href="#popularcause" class="btn btn-outline-primary">
+                        View Causes
+                    </a>
+                </div><!-- .xs-welcome-wraper END -->
+            </div><!-- .container end -->
+            <div class="xs-black-overlay"></div>
+        </div></div></div></div><div class="owl-nav disabled"><div class="owl-prev"><i class="fa fa-angle-left xs-round-nav"></i></div><div class="owl-next"><i class="fa fa-angle-right xs-round-nav"></i></div></div><div class="owl-dots disabled"></div></div>
+</section>
---
+++ src/data/Charities.js
@@ -0,0 +1,18 @@
+module.exports = {
+    charities: [
+        {
+            id: 1,
+            title: 'Frist Charity Project',
+            category: 'Money',
+            thumbnail: 'https://img.idxchannel.com/media/700/images/idx/2021/01/27/uang.jpg',
+            pledged: 99000,
+            target: 100000,
+            date_end: +new Date('10 August 2022'),
+            profile_foto: 'https://spesialis1.orthopaedi.fk.unair.ac.id/wp-content/uploads/2021/02/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg',
+            profile_name: 'Viroide Bueno',
+            no_pledges: 0
+
+        }
+
+    ]
+}
